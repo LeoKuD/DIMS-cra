a89ec541ce8efb15a6ebe29893c557e581154848
maximsan <maximuzz@yandex.ru>
2020-12-08 21:14:45 +0300

    fix: test run, update CRA

--- src/components/Buttons/Button/Button.test.jsx
+++ src/components/Buttons/Button/Button.test.jsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { Button } from './Button';
 
 describe('Button', () => {
-  it('should show  ', () => {
+  it('should show passed text', () => {
     const { getByText } = render(<Button>Hello DIMS!</Button>);
     const buttonText = getByText(/hello dims/i);
 
